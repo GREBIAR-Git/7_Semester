017654509caec87dbabc5a8bf5135e19057be9a4
grebiar <nik17092001@mail.ru>
2022-10-14 14:10:22 +0300

    fix

--- Микроконтроллеры/Лаба 1/Lab1/Lab1/main.c
+++ Микроконтроллеры/Лаба 1/Lab1/Lab1/main.c
@@ -1,20 +1,45 @@
 #include <avr/io.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+void but1(bool*,bool*);
 
 int main(void)
 {
-	//DDRD = 0; // 0x
-	//PORTB = 0; // 0b
-	//PORTD = 1; // 0b
+	DDRD = 1; // 0x
+	DDRB = 0; // 0x
+	PORTB = 0; // 0b
+	PORTD = 1; // 0b
+	bool on=false;
+	bool flag = false;
 	while (1)
 	{
-		if (PINB & 1)
+		but1(&flag,&on);
+	}
+}
+
+void but1(bool* flag, bool* on)
+{
+	if(PINB & 1)
+	{
+		if ((*flag)==false)
 		{
-			PORTD = 0;
+			(*flag) = true;
+			(*on) = !(*on);
 		}
-		else
-		{
-			PORTD = 1;
-		}
+	}
+	else
+	{
+		(*flag) = false;
+	}
+	
+	if (*on)
+	{
+		PORTD = 1;
+	}
+	else
+	{
+		PORTD = 0;
 	}
 }
 
