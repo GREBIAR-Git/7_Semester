440c7186edab0d12907b77d9e4584870f13bc99f
mikhail <mikhail.allyanov@gmail.com>
2022-11-30 13:40:53 +0300

    .

--- Оптимизация (Лукьянов)/lab3/lab3/Program.cs
+++ Оптимизация (Лукьянов)/lab3/lab3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lab3
 {
@@ -13,29 +10,28 @@ namespace lab3
 
         static Data[,] data;
 
-        static int count = 0;
+        static Dictionary<int, double> speeds = new Dictionary<int, double>();
+        static Dictionary<Point, double> times = new Dictionary<Point, double>();
+
 
         static void Go(int r, int h)
         {
-            for (int i = r + 1; i < m; i++)//горизонталь
+            // перебираем все точки
+            for (int i = 0; i <= n; i++) // i-й столбец
             {
-                for (int j = h; j < i + 1 && j <= n; j++)//вертикаль
+                for (int j = 1; j <= m; j++) // j-я строка
                 {
-                    double speed = data[r, h].Speed;
-                    double time = CalculateTime(new Point(j, i), new Point(h, r), ref speed);
-                    if (data[i, j].Time > time + data[r, h].Time)
+                    // для каждой точки ищем минимальное время, за которое в нее можно дойти
+                    // перебираем все точки до нее (предыдущие)
+                    for (int a = 0; a <= i; a++) // a-й столбец, <= т.к. можно ровно вниз
                     {
-                        data[i, j].Time = time + data[r, h].Time;
-                        data[i, j].Speed = speed;
-                        Go(i, j);
+                        for (int b = a; b < j; b++) // b-я строка, < т.к. нельзя горизонтально идти (по той же строке)
+                        {
+                            // считаем время
+                            CalculateTime(new Point(i, j), new Point(a, b));
+                        }
                     }
                 }
-            }
-
-            double lastTime = CalculateTime(new Point(n, m), new Point(h, r), data[r, h].Speed);
-            if (data[m, n].Time > lastTime + data[r, h].Time)
-            {
-                data[m, n].Time = lastTime + data[r, h].Time;
             }
         }
 
@@ -44,6 +40,7 @@ namespace lab3
             string[] str = Console.ReadLine().Split(' ');
             n = byte.Parse(str[0]);
             m = byte.Parse(str[1]);
+            //times[new Point(0, 0)] = 0;
             data = new Data[n + 1, m + 1];
             data[0, 0] = new Data(0, 0);
             for (int i = 1; i < m; i++)//горизонталь
@@ -54,55 +51,29 @@ namespace lab3
                 }
             }
             data[m, n] = new Data();
-
             Go(0, 0);
-            Console.WriteLine("count: " + count);
-            Console.WriteLine("answer: " + data[n, m].Time);
-            PrintData();
+            Console.WriteLine(times[new Point(n, m)]);
         }
 
-        static void PrintData()
+        static void CalculateTime(Point current, Point prev)
         {
-            Console.WriteLine();
-            for (int i = 0; i <= n; i++)
+            if (!speeds.ContainsKey(prev.Y))
             {
-                string str = string.Empty;
-                for (int j = 0; j <= m; j++)
-                {
-                    if (data[i, j] == null || data[i, j].Time == double.MaxValue)
-                    {
-                        str = "0,000 " + str;
-                    }
-                    else
-                    {
-                        str = string.Format("{0:0.000}", Math.Round(data[i, j].Time, 3)) + " " + str;
-                    }
-                }
-                Console.WriteLine(str);
+                speeds.Add(prev.Y, Math.Sqrt(2 * g * prev.Y));
             }
-            Console.WriteLine();
-        }
+            double speed = speeds[prev.Y];
 
-        static double CalculateTime(Point current, Point prev, ref double speed)
-        {
-            count++;
             double y1 = current.Y - prev.Y;
             double plank_length = Math.Sqrt(Math.Pow(current.X - prev.X, 2) + Math.Pow(y1, 2));
             double cos_a = y1 / plank_length;
             double g1 = g * cos_a;
             double speed1 = (Math.Sqrt(Math.Pow(speed, 2) + 2 * g1 * plank_length) - speed);
-            speed += speed1;
-            return speed1 / g1;
-        }
+            double time = times[prev] + speed1 / g1;
 
-        static double CalculateTime(Point current, Point prev, double speed)
-        {
-            count++;
-            double y1 = current.Y - prev.Y;
-            double plank_length = Math.Sqrt(Math.Pow(current.X - prev.X, 2) + Math.Pow(y1, 2));
-            double cos_a = y1 / plank_length;
-            double g1 = g * cos_a;
-            return (Math.Sqrt(Math.Pow(speed, 2) + 2 * g1 * plank_length) - speed) / g1;
+            if (!times.ContainsKey(current) || time < times[current])
+            {
+                times[current] = time;
+            }
         }
     }
 
